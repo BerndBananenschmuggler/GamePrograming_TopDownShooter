e7523d35660f4becaeba748d519604235051e7bf
Jonas <jonas.gerber@gmx.de>
2023-07-23 05:38:26 +0200

    Enemies move further towards Player if Player is not in sight

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class EnemyController : MonoBehaviour
 {
     public enum States { Move, Attack }
     public States State;
 
+    public LayerMask HurtboxLayermask;
 
     [SerializeField] private float _attackRange = 5f;
     [SerializeField] private RangeWeapon _equippedWeapon;
@@ -19,6 +21,12 @@ public class EnemyController : MonoBehaviour
 
     private Transform _playerTransform;
     private float _distanceToPlayer = float.MaxValue;
+    private LayerMask _playerSightScanIgnoreLayermask;
+
+    private void Awake()
+    {
+        _playerSightScanIgnoreLayermask = LayerMask.NameToLayer("Hurtbox");
+    }
 
     private void Start()
     {
@@ -35,7 +43,9 @@ public class EnemyController : MonoBehaviour
         // Maybe turn towards player when firing dont care while moving
         //if(|| Vector3.Angle(transform.position, _playerTransform.position) > MAX_ATTACK_ANGLE)
 
-        if (_distanceToPlayer > _attackRange)
+        Debug.Log($"Is Player visible: {IsPlayerVisibleFromPosition(transform.position)}");
+
+        if (_distanceToPlayer > _attackRange || IsPlayerVisibleFromPosition(transform.position) == false)
         {
             Move();
             State = States.Move;
@@ -52,6 +62,12 @@ public class EnemyController : MonoBehaviour
     {
         Handles.color = Color.yellow;
         Handles.DrawWireDisc(transform.position, transform.up, _attackRange);
+
+        if(Application.isPlaying )
+        {
+            Handles.color = Color.red;
+            Handles.DrawLine(_playerTransform.position, transform.position, 3f);
+        }        
     }
 
     private void Move()
@@ -68,6 +84,25 @@ public class EnemyController : MonoBehaviour
 
     private void Attack()
     {
+        // Aim Phase
+        // - Check if ray between transform and playerTransform hits anything but the player -> Player in sight,
+        // - if not in sight -> Move further until in sight
 
+        // Fire Phase
+        // - Use the weapon once
+
+        // Repeat until State is not attack
+    }
+
+    private bool IsPlayerVisibleFromPosition(Vector3 position)
+    {
+        RaycastHit hit;
+        //LayerMask ignoreMask = LayerMask.NameToLayer("Hurtbox");
+        // ~(1 << _playerSightScanIgnoreLayermask) -> Reverse of the Layer that should be ignored is accepted as Raycast targets
+        Physics.Raycast(position, _playerTransform.position - position, out hit, float.MaxValue, ~(1 << _playerSightScanIgnoreLayermask));
+        Debug.Log($"Hit layer: {LayerMask.LayerToName(hit.collider.gameObject.layer)}");
+        //Debug.Log($"Hit name: {hit.collider.name}");
+
+        return hit.collider.gameObject.layer == LayerMask.NameToLayer("Player");
     }
 }
